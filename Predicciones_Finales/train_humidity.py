e25b0ceea8494477044e22e96c4c9e42ebec472b
gaiborjosue <gaibor.edward@cebi.edu.ec>
2021-09-08 20:02:33 -0500

    3rd_release

--- Predicciones_Finales/train_humidity.py
+++ Predicciones_Finales/train_humidity.py
@@ -6,7 +6,7 @@ from sklearn import linear_model
 import joblib
 
 df = pd.read_csv(
-    "Predicciones_Finales\\Data\\th_station_2nd_data.csv", delimiter=",")
+    "Predicciones_Finales\\Data\\th_station_3rd_data.csv", delimiter=",")
 
 x = df[["field1"]]
 y = df[["field2"]]
@@ -16,7 +16,7 @@ X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
 reg = linear_model.LinearRegression()
 reg.fit(X_train, y_train)
 
-joblib.dump(reg, 'Predicciones_Finales\\Models\\2nd_release\\hum_predictor.joblib')
+joblib.dump(reg, 'Predicciones_Finales\\Models\\3rd_release\\hum_predictor.joblib')
 
 # predict_hum = reg.predict(X_test)
 # print(predict_hum)
